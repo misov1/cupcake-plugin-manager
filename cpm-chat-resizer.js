af07db6af1338740c0c984e71d01be9ebd213329
ruyari <eriya9694@gmail.com>
2026-02-24 13:06:29 +0900

    Add @update-url to chat-resizer, fix version format to semver

--- cpm-chat-resizer.js
+++ cpm-chat-resizer.js
@@ -1,8 +1,9 @@
 //@name CPM Component - Chat Input Resizer
 //@display-name Cupcake UI Resizer
 //@api 3.0
-//@version 0.1
+//@version 0.1.0
 //@author Cupcake
+//@update-url https://raw.githubusercontent.com/ruyari-cupcake/cupcake-plugin-manager/main/cpm-chat-resizer.js
 
 /**
  * ======== CUPCAKE PM Sub-Plugin: Chat Input Resizer ========
